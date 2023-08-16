82e6f5f2d0344ccb9b654d13a3b857e532e2df48
Phayruzee <faruqadeniran05@gmail.com>
2023-08-16 08:29:00 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,9 +14,10 @@ void jack_bauer(void)
 		{
 			_putchar(i / 10 + '0');
 			_putchar(i % 10 + '0');
-			_putchar(';');
+			_putchar(':');
 			_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
