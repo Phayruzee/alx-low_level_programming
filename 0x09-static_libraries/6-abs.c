694fad85fa05f3122da56c9647f7d283077255e0
Phayruzee <faruqadeniran05@gmail.com>
2023-09-01 19:13:30 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int_abs - function to find the value of a number
+ * @a: function parameters
+ * Return: -a or a;
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+	{
+		return (a);
+	}
+}
+
