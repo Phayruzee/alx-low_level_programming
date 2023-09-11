932a21957dc9657a8fd7727d62ff02685ad4903f
Phayruzee <faruqadeniran05@gmail.com>
2023-09-11 09:36:54 -0700

    alx-low_level_programing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - A program that prints the alphabets in lowercase
+ * Return: always 0 (successs)
+ */
+int main(void)
+{
+	char d;
+	
+	d = 'a';
+	while
+		(d <= 'z'){
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
