6e801a11f4af78bb8405c423a129b5425e362319
Phayruzee <faruqadeniran05@gmail.com>
2023-10-26 12:36:46 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,41 +1,29 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: Pointer to a string of 0 and 1 chars.
- * Return: The converted number, or 0 if
- * there is one or more chars in the string b that is not 0 or 1 or b is NULL.
+ * binary_to_uint -converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return:converted number, or 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
-    int base = 1;
-    const char *ptr;
+	int i;
+	unsigned int j;
 
-    /*Check if the string is NULL*/
-    if (!b)
-        return (0);
-
-    /*Move the pointer to the end of the string to start from LSB*/
-    ptr = b;
-    while (*ptr)
-        ptr++;
-    ptr--;
-
-    /*Convert the binary to unsigned int*/
-    for (; ptr >= b; ptr--)
-    {
-        if (*ptr != '0' && *ptr != '1')
-	       	/*Check for invalid chars*/
-            return (0);
-
-        if (*ptr == '1')
-            num += base;
-
-        base *= 2;
-    }
-    
-    return (num);
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
 }
-
